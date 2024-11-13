6e9a4b8b8e49a55c65203ae0233fd969aa49df56
Kryštof Čejchan <kcejchan@koop.cz>
2024-11-13 20:44:51 +0100

    3

---
+++ src/main/java/cz/krystofcejchan/semaphore_lock_await/SynchronizedExample.java
@@ -0,0 +1,43 @@
+package cz.krystofcejchan.semaphore_lock_await;
+
+class MyCounter2 {
+    private int count = 0;
+
+    // synchronized metoda pro zajištění bezpečného přístupu
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+
+public class SynchronizedExample {
+    public static void main(String[] args) throws InterruptedException {
+        MyCounter2 counter = new MyCounter2();
+
+        // vytvoření dvou vláken, která volají metodu increment
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+        
+        // počká na dokončení obou vláken
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Count: " + counter.getCount()); // Výstup: 2000
+    }
+}
